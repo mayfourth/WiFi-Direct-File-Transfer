f4ac98c559aaeaffd0c20ac9f4ea68aabb26666a
Teja Pitla <teja.pitla@gmail.com>
2012-08-07 18:02:12 -0700

    Initial project added, basic UI being implemented

---
+++ WiFiDirectFileTransfer/gen/edu/pdx/cs410/wifi/direct/file/transfer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.pdx.cs410.wifi.direct.file.transfer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
