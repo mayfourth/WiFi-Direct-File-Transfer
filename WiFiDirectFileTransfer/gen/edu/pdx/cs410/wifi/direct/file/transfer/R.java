1a6b929cd7cd7dcc9dfb811690f307134838553f
Teja Pitla <teja.pitla@gmail.com>
2012-08-07 18:40:50 -0700

    Main activity UI finished

--- WiFiDirectFileTransfer/gen/edu/pdx/cs410/wifi/direct/file/transfer/R.java
+++ WiFiDirectFileTransfer/gen/edu/pdx/cs410/wifi/direct/file/transfer/R.java
@@ -15,7 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int server_file_path=0x7f070000;
+        public static final int server_search_results_list=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- WiFiDirectFileTransfer/src/edu/pdx/cs410/wifi/direct/file/transfer/MainActivity.java
+++ WiFiDirectFileTransfer/src/edu/pdx/cs410/wifi/direct/file/transfer/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+//test
+
 public class MainActivity extends Activity {
 
     @Override
